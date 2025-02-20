9cda2526bf3c182fc259700c9ed79759b1d737cf
sergiohlazaro <sergio.hlazaro@alumnos.upm.es>
2025-02-20 11:48:59 +0100

    Prueba del gitbranch error.

--- script_trabajo1_v2.py
+++ script_trabajo1_v2.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.preprocessing import MinMaxScaler
-
+# Prueba del gitbranch
 # Cargar el dataset
 df = pd.read_csv("incidents.byCountryYr.csv")
 
